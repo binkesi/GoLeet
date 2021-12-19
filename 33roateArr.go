89cb19fe466fe51868ed4d593ae651bd99e41588
binkesi <sjtusgn@163.com>
2021-12-19 17:38:32 +0800

    solve question 33

---
+++ 33roateArr.go
@@ -0,0 +1,23 @@
+// https://leetcode-cn.com/problems/search-in-rotated-sorted-array/
+package goleet
+
+func search(nums []int, target int) int {
+	var in int
+	length := len(nums)
+	if length == 1 {
+		if nums[0] == target {
+			return 0
+		}
+		return -1
+	}
+	for i := 0; i < length; i++ {
+		if target == nums[i] {
+			in = i
+			break
+		}
+		if i == length-1 {
+			return -1
+		}
+	}
+	return in
+}
