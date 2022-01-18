7e8afbf9b7bbf0880575fdc179f9223940ae1118
binkesi <sjtusgn@163.com>
2022-01-18 22:08:08 +0800

    solve prob 23 merge lists one by one

--- 23klinkList.go
+++ 23klinkList.go
@@ -17,12 +17,34 @@ func mergeKLists(lists []*ListNode) *ListNode {
 	}
 	res = lists[0]
 	for i := 1; i < lenL; i++ {
-		res = mergeTwoList(res, lists[i])
+		res = mertwo(res, lists[i])
 	}
 	return res
 }
 
-func mergeTwoList(l1 *ListNode, l2 *ListNode) *ListNode {
+func mertwo(l1, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
 	var res *ListNode = &ListNode{}
-
+	tmp := res
+	for l1 != nil && l2 != nil {
+		if l1.Val <= l2.Val {
+			tmp.Next = l1
+			l1 = l1.Next
+		} else {
+			tmp.Next = l2
+			l2 = l2.Next
+		}
+		tmp = tmp.Next
+	}
+	if l1 == nil {
+		tmp.Next = l2
+	} else if l2 == nil {
+		tmp.Next = l1
+	}
+	return res.Next
 }
