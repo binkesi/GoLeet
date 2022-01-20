15b89953840f42bbf0a5c3e369e6889c1066e037
binkesi <sjtusgn@163.com>
2022-01-20 16:47:30 +0800

    solve 116 one level to another

---
+++ 116connectNode2.go
@@ -0,0 +1,25 @@
+package goleet
+
+// https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node/
+
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
+}
+
+func connect(root *Node) *Node {
+	if root == nil {
+		return root
+	}
+	for node := root; node.Left != nil; node = node.Left {
+		tmp := node
+		for ; tmp.Next != nil; tmp = tmp.Next {
+			tmp.Left.Next = tmp.Right
+			tmp.Right.Next = tmp.Next.Left
+		}
+		tmp.Left.Next = tmp.Right
+	}
+	return root
+}
