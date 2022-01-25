29b44188cec607fdf0fcfcba802a01761a74402c
binkesi <sjtusgn@163.com>
2022-01-25 11:24:43 +0800

    solve prob 130 use bfs

--- graph/130roundArea.go
+++ graph/130roundArea.go
@@ -5,14 +5,14 @@ package graph
 type pair struct{ x, y int }
 
 func solve(board [][]byte) {
-	row, col := len(board)-2, len(board[0])-2
+	row, col := len(board), len(board[0])
 	visited := make([][]bool, row)
 	for i := range visited {
 		visited[i] = make([]bool, col)
 	}
 	for i := 1; i < row; i++ {
 		for j := 1; j < col; j++ {
-			if board[i][j] == 'X' || visited[i-1][j-1] {
+			if board[i][j] == 'X' || visited[i][j] {
 				continue
 			} else {
 				bfsA(board, i, j, visited)
@@ -22,55 +22,46 @@ func solve(board [][]byte) {
 }
 
 func bfsA(board [][]byte, row, col int, visited [][]bool) {
+	alone := true
 	queue := []pair{{row, col}}
-	visited[row-1][col-1] = true
+	visited[row][col] = true
 	for i := 0; i < len(queue); i++ {
 		p := queue[i]
 		r, c := p.x, p.y
-		if board[r-1][c] == 'O' {
-			if row-1 == 0 {
-				for _, v := range queue {
-					board[v.x][v.y] = 'O'
-				}
-				return
-			} else if !visited[r-2][c-1] {
-				visited[r-2][c-1] = true
-				queue = append(queue, pair{r - 1, c})
-			}
+		if r == 0 || r == len(board)-1 || c == 0 || c == len(board[0])-1 {
+			alone = false
 		}
-		if board[r][c-1] == 'O' {
+		if r-1 >= 0 && board[r-1][c] == 'O' && !visited[r-1][c] {
+			if r-1 == 0 {
+				alone = false
+			}
+			visited[r-1][c] = true
+			queue = append(queue, pair{r - 1, c})
+		}
+		if c-1 >= 0 && board[r][c-1] == 'O' && !visited[r][c-1] {
 			if c-1 == 0 {
-				for _, v := range queue {
-					board[v.x][v.y] = 'O'
-				}
-				return
-			} else if !visited[r-1][c-2] {
-				visited[r-1][c-2] = true
-				queue = append(queue, pair{r, c - 1})
+				alone = false
 			}
+			visited[r][c-1] = true
+			queue = append(queue, pair{r, c - 1})
 		}
-		if board[r+1][c] == 'O' {
-			if row+1 == len(board)-1 {
-				for _, v := range queue {
-					board[v.x][v.y] = 'O'
-				}
-				return
-			} else if !visited[r][c-1] {
-				visited[r][c-1] = true
-				queue = append(queue, pair{r + 1, c})
+		if r+1 <= len(board)-1 && board[r+1][c] == 'O' && !visited[r+1][c] {
+			if r+1 == len(board)-1 {
+				alone = false
 			}
+			visited[r+1][c] = true
+			queue = append(queue, pair{r + 1, c})
 		}
-		if board[r][c+1] == 'O' {
-			if col+1 == len(board[0]) {
-				for _, v := range queue {
-					board[v.x][v.y] = 'O'
-				}
-				return
-			} else if !visited[r-1][c] {
-				visited[r-1][c] = true
-				queue = append(queue, pair{r, c + 1})
+		if c+1 <= len(board[0])-1 && board[r][c+1] == 'O' && !visited[r][c+1] {
+			if c+1 == len(board[0])-1 {
+				alone = false
 			}
+			visited[r][c+1] = true
+			queue = append(queue, pair{r, c + 1})
 		}
+	}
+	if !alone {
+		return
 	}
 	for _, v := range queue {
 		board[v.x][v.y] = 'X'
