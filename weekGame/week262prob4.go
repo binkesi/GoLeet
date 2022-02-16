6a0a98038b4eb91cb7d6cc64dc72d73329bfae90
binkesi <sjtusgn@163.com>
2022-02-16 10:03:13 +0800

    solve week 262 prob4 use divide two

--- weekGame/week262prob4.go
+++ weekGame/week262prob4.go
@@ -1,6 +1,10 @@
 package weekgame
 
-import "math"
+import (
+	"math"
+	"math/bits"
+	"sort"
+)
 
 // https://leetcode-cn.com/problems/partition-array-into-two-arrays-to-minimize-sum-difference/
 
@@ -13,31 +17,53 @@ func minimumDifference2(nums []int) (ans int) {
 	a, b := nums[0:n], nums[n:]
 	ak, bk := make([][]int, n+1), make([][]int, n+1)
 	ak[0], bk[0] = []int{0}, []int{0}
-	for i := 1; i <= n; i++ {
-		for _, v := range ak[i-1] {
-			for _, num := range a {
-				ak[i] = append(ak[i], v+num)
+	stats := 1 << n
+	for i := 0; i < stats; i++ {
+		tmp := i
+		cnt := bits.OnesCount(uint(tmp))
+		resA, resB := 0, 0
+		for j := n - 1; tmp > 0; j-- {
+			if tmp&1 == 1 {
+				resA += a[j]
+				resB += b[j]
 			}
+			tmp = tmp >> 1
 		}
-	}
-	for i := 1; i <= n; i++ {
-		for _, v := range bk[i-1] {
-			for _, num := range b {
-				bk[i] = append(bk[i], v+num)
-			}
-		}
+		ak[cnt] = append(ak[cnt], resA)
+		bk[cnt] = append(bk[cnt], resB)
 	}
 	ans = math.MaxInt32
 	for j := 0; j <= n; j++ {
 		l, r := ak[j], bk[n-j]
+		sort.Ints(r)
+		lr := len(r)
 		for _, v1 := range l {
-			for _, v2 := range r {
-				tmp := abs(sum - 2*(v1+v2))
-				if tmp < ans {
-					ans = tmp
+			left, right := 0, lr-1
+			for left < right-1 {
+				mid := left + (right-left)/2
+				tmp := sum - 2*(v1+r[mid])
+				if tmp == 0 {
+					ans = 0
+					return
+				}
+				if tmp > 0 {
+					left = mid
+				} else if tmp < 0 {
+					right = mid
 				}
 			}
+			ans = minNum(ans, abs(sum-2*(v1+r[left])), abs(sum-2*(v1+r[right])))
 		}
 	}
 	return
+}
+
+func minNum(a, b, c int) int {
+	if a <= b && a <= c {
+		return a
+	}
+	if b <= a && b <= c {
+		return b
+	}
+	return c
 }
