000554035e421ba4d3f26f69e3b59a53c8916e68
binkesi <sjtusgn@163.com>
2022-02-17 11:33:56 +0800

    solve prob 688 using dp

--- dynamicProcess/688knightProb.go
+++ dynamicProcess/688knightProb.go
@@ -1,5 +1,24 @@
 package dynamicprocess
 
 func knightProbability(n int, k int, row int, column int) float64 {
-
+	dp := make([][][]float64, k+1)
+	dirs := [][2]int{{-2, 1}, {-1, 2}, {1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}}
+	for step := range dp {
+		dp[step] = make([][]float64, n)
+		for i := 0; i < n; i++ {
+			dp[step][i] = make([]float64, n)
+			for j := 0; j < n; j++ {
+				if step == 0 {
+					dp[step][i][j] = 1
+				} else {
+					for _, v := range dirs {
+						if x, y := i+v[0], j+v[1]; x >= 0 && y >= 0 && x < n && y < n {
+							dp[step][i][j] += dp[step-1][x][y] / 8
+						}
+					}
+				}
+			}
+		}
+	}
+	return dp[k][row][column]
 }
