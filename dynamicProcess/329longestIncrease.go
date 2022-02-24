6491fbdbc6bb5faa62243f04e32e6c30c52f93a7
binkesi <sjtusgn@163.com>
2022-02-24 22:17:07 +0800

    solve prob 329 using dfs

--- dynamicProcess/329longestIncrease.go
+++ dynamicProcess/329longestIncrease.go
@@ -2,45 +2,46 @@ package dynamicprocess
 
 // https://leetcode-cn.com/problems/longest-increasing-path-in-a-matrix/
 
-func longestIncreasingPath(matrix [][]int) (ans int) {
-	r, c := len(matrix), len(matrix[0])
-	var bfs func(int, int, [][]int, *int)
-	dirs := [][2]int{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}
-	bfs = func(i, j int, graph [][]int, res *int) {
-		r, c := len(graph), len(graph[0])
-		queue := [][3]int{}
-		queue = append(queue, [3]int{i, j, 1})
-		for len(queue) != 0 {
-			p := queue[0]
-			x, y, step := p[0], p[1], p[2]
-			queue = queue[1:]
-			for _, v := range dirs {
-				nx, ny := x+v[0], y+v[1]
-				if nx >= 0 && ny >= 0 && nx < r && ny < c && graph[nx][ny] > graph[x][y] {
-					queue = append(queue, [3]int{nx, ny, step + 1})
-				}
-			}
-			if len(queue) == 0 {
-				if *res < step {
-					*res = step
-				}
-			}
+var (
+	dirs          = [][]int{[]int{-1, 0}, []int{1, 0}, []int{0, -1}, []int{0, 1}}
+	rows, columns int
+)
+
+func longestIncreasingPath(matrix [][]int) int {
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
+		return 0
+	}
+	rows, columns = len(matrix), len(matrix[0])
+	memo := make([][]int, rows)
+	for i := 0; i < rows; i++ {
+		memo[i] = make([]int, columns)
+	}
+	ans := 0
+	for i := 0; i < rows; i++ {
+		for j := 0; j < columns; j++ {
+			ans = max(ans, dfs(matrix, i, j, memo))
 		}
 	}
-	for i := 0; i < r; i++ {
-		for j := 0; j < c; j++ {
-			for ind, v := range dirs {
-				nx, ny := i+v[0], i+v[1]
-				if nx >= 0 && ny >= 0 && nx < r && ny < r {
-					if matrix[nx][ny] < matrix[i][j] {
-						break
-					}
-				}
-				if ind == 3 {
-					bfs(i, j, matrix, &ans)
-				}
-			}
+	return ans
+}
+
+func dfs(matrix [][]int, row, column int, memo [][]int) int {
+	if memo[row][column] != 0 {
+		return memo[row][column]
+	}
+	memo[row][column]++
+	for _, dir := range dirs {
+		newRow, newColumn := row+dir[0], column+dir[1]
+		if newRow >= 0 && newRow < rows && newColumn >= 0 && newColumn < columns && matrix[newRow][newColumn] > matrix[row][column] {
+			memo[row][column] = max(memo[row][column], dfs(matrix, newRow, newColumn, memo)+1)
 		}
 	}
-	return
+	return memo[row][column]
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
 }
