2a8f83e7243af1d438e75e162aa5ca65561035e1
binkesi <sjtusgn@163.com>
2022-03-12 10:50:40 +0800

    solve prob 590 using iteration

--- binaryTree/590nPostorder.go
+++ binaryTree/590nPostorder.go
@@ -21,3 +21,27 @@ func postorder(root *NodeN) (ans []int) {
 	post(root)
 	return
 }
+
+func postorder2(root *NodeN) (ans []int) {
+	if root == nil {
+		return
+	}
+	type Cnode struct {
+		node *NodeN
+		num  int
+	}
+	nStack := []Cnode{{root, 0}}
+	for len(nStack) != 0 {
+		n := len(nStack)
+		top := nStack[n-1]
+		nChild := len(top.node.Children)
+		if nChild == 0 || top.num == nChild {
+			ans = append(ans, top.node.Val)
+			nStack = nStack[0 : n-1]
+		} else {
+			nStack[n-1].num++
+			nStack = append(nStack, Cnode{top.node.Children[top.num], 0})
+		}
+	}
+	return
+}
