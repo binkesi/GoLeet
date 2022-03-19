6ecbad2148a7e439b7992c4abdda6d46982098aa
binkesi <sjtusgn@163.com>
2022-03-19 13:14:14 +0800

    review prob 347 heap

--- priorityQueue/347topkFrequent.go
+++ priorityQueue/347topkFrequent.go
@@ -5,36 +5,31 @@ import "container/heap"
 // https://leetcode-cn.com/problems/top-k-frequent-elements/
 
 func topKFrequent(nums []int, k int) (ans []int) {
-	occurrences := map[int]int{}
-	for _, num := range nums {
-		occurrences[num]++
+	nMap := make(map[int]int)
+	for _, v := range nums {
+		nMap[v] += 1
 	}
-	var lheap IHeap
-	for key, value := range occurrences {
-		lheap = append(lheap, [2]int{key, value})
+	numHeap := lowHeap{}
+	for k, v := range nMap {
+		heap.Push(&numHeap, [2]int{k, v})
 	}
-	heap.Init(&lheap)
 	for i := 0; i < k; i++ {
-		res := heap.Pop(&lheap)
-		ans = append(ans, res.([2]int)[0])
+		num := heap.Pop(&numHeap).([2]int)
+		ans = append(ans, num[0])
 	}
 	return
 }
 
-type IHeap [][2]int
+type lowHeap [][2]int
 
-func (h IHeap) Len() int           { return len(h) }
-func (h IHeap) Less(i, j int) bool { return h[i][1] > h[j][1] }
-func (h IHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-
-func (h *IHeap) Push(x interface{}) {
-	*h = append(*h, x.([2]int))
-}
-
-func (h *IHeap) Pop() interface{} {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-	return x
+func (l lowHeap) Len() int              { return len(l) }
+func (l lowHeap) Less(i, j int) bool    { return l[i][1] > l[j][1] }
+func (l lowHeap) Swap(i, j int)         { l[i], l[j] = l[j], l[i] }
+func (l *lowHeap) Push(num interface{}) { *l = append(*l, num.([2]int)) }
+func (l *lowHeap) Pop() interface{} {
+	old := *l
+	n := old.Len()
+	top := old[n-1]
+	*l = old[:n-1]
+	return top
 }
