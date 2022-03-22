159ac5c010c625fcbd3fd0d0133bd09d39cac89d
binkesi <sjtusgn@163.com>
2022-03-22 12:27:34 +0800

    solve prob 1208 using sliding window

---
+++ slidingWindow/1208equalSubstring.go
@@ -0,0 +1,35 @@
+package slidingwindow
+
+// https://leetcode-cn.com/problems/get-equal-substrings-within-budget/
+
+func equalSubstring(s string, t string, maxCost int) (ans int) {
+	n := len(s)
+	sDif := make([]int, n)
+	for i := 0; i < n; i++ {
+		sDif[i] = abs(int(s[i]) - int(t[i]))
+	}
+	for i := 1; i < n; i++ {
+		sDif[i] = sDif[i-1] + sDif[i]
+	}
+	sDif = append([]int{0}, sDif...)
+	for i, j := 0, 1; j < n+1; {
+		sum := sDif[j] - sDif[i]
+		if sum <= maxCost {
+			if j-i > ans {
+				ans = j - i
+			}
+			j++
+		} else {
+			i++
+		}
+	}
+	return
+}
+
+func abs(num int) int {
+	if num >= 0 {
+		return num
+	} else {
+		return -num
+	}
+}
