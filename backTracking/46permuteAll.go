d5c5e59085e85e1e7259c02404dd04885fd55ce1
binkesi <sjtusgn@163.com>
2022-03-23 13:02:13 +0800

    solve prob 46 using backtracking

--- backTracking/46permuteAll.go
+++ backTracking/46permuteAll.go
@@ -33,3 +33,34 @@ func putNums(nums, curArr []int, res *[][]int) {
 		putNums(nums, tmp, res)
 	}
 }
+
+func permute2(nums []int) (ans [][]int) {
+	n := len(nums)
+	var chooseNum func(ch []bool, cur []int)
+	chooseNum = func(ch []bool, cur []int) {
+		l := len(cur)
+		if l == n {
+			ans = append(ans, cur)
+			return
+		}
+		for i, v := range ch {
+			if !v {
+				nch := make([]bool, n)
+				ncur := make([]int, l)
+				copy(nch, ch)
+				copy(ncur, cur)
+				nch[i] = true
+				ncur = append(ncur, nums[i])
+				chooseNum(nch, ncur)
+			}
+		}
+	}
+	for i := range nums {
+		choice := make([]bool, n)
+		current := make([]int, 0)
+		choice[i] = true
+		current = append(current, nums[i])
+		chooseNum(choice, current)
+	}
+	return
+}
