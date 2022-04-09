cca80ee8a69e032dcbaa10194852c515e4b21739
binkesi <sjtusgn@163.com>
2022-04-09 11:16:46 +0800

    solve prob 780 using math

---
+++ mathGame/780reachPoints.go
@@ -0,0 +1,23 @@
+package mathgame
+
+// https://leetcode-cn.com/problems/reaching-points/solution/
+
+func reachingPoints(sx, sy, tx, ty int) bool {
+	for tx > sx && ty > sy && tx != ty {
+		if tx > ty {
+			tx %= ty
+		} else {
+			ty %= tx
+		}
+	}
+	switch {
+	case tx == sx && ty == sy:
+		return true
+	case tx == sx:
+		return ty > sy && (ty-sy)%tx == 0
+	case ty == sy:
+		return tx > sx && (tx-sx)%ty == 0
+	default:
+		return false
+	}
+}
