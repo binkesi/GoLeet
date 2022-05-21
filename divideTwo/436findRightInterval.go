746b05658dd10b46eabbbf86b50760b6b4646195
binkesi <sjtusgn@163.com>
2022-05-21 12:57:10 +0800

    modify prob 436

--- divideTwo/436findRightInterval.go
+++ divideTwo/436findRightInterval.go
@@ -6,12 +6,12 @@ import "sort"
 
 func findRightInterval(intervals [][]int) (ans []int) {
 	iArr := [][2]int{}
-	var find func(int, [][2]int) [2]int
-	find = func(a int, arr [][2]int) [2]int {
+	var find func(int, [][2]int) int
+	find = func(a int, arr [][2]int) int {
 		n := len(arr)
 		l, r := 0, n-1
 		if arr[r][1] < a {
-			return [2]int{-1, -1}
+			return -1
 		}
 		for r > l {
 			mid := l + (r-l)>>1
@@ -21,15 +21,14 @@ func findRightInterval(intervals [][]int) (ans []int) {
 				r = mid
 			}
 		}
-		return arr[l]
+		return arr[l][0]
 	}
 	for i, v := range intervals {
 		iArr = append(iArr, [2]int{i, v[0]})
 	}
 	sort.Slice(iArr, func(i, j int) bool { return iArr[i][1] < iArr[j][1] })
 	for _, v := range intervals {
-		res := find(v[1], iArr)
-		ans = append(ans, res[0])
+		ans = append(ans, find(v[1], iArr))
 	}
 	return
 }
