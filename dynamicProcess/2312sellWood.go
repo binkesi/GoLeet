5aa663ebf13030e809a020ba56968dfe47625bd3
binkesi <sjtusgn@163.com>
2022-07-16 13:50:42 +0800

    optimize prob 2312

--- dynamicProcess/2312sellWood.go
+++ dynamicProcess/2312sellWood.go
@@ -1,24 +1,21 @@
 package dynamicprocess
 
+// https://leetcode.cn/problems/selling-pieces-of-wood/
+
 func sellingWood(m int, n int, prices [][]int) int64 {
-	pr := make([][]int, m+1)
-	for i := range pr {
-		pr[i] = make([]int, n+1)
-	}
-	for _, price := range prices {
-		pr[price[0]][price[1]] = price[2]
-	}
 	dp := make([][]int64, m+1)
 	for i := range dp {
 		dp[i] = make([]int64, n+1)
 	}
+	for _, price := range prices {
+		dp[price[0]][price[1]] = int64(price[2])
+	}
 	for i := 1; i <= m; i++ {
 		for j := 1; j <= n; j++ {
-			dp[i][j] = int64(pr[i][j])
-			for k := 1; k < j; k++ {
+			for k := 1; k <= j/2; k++ {
 				dp[i][j] = max64(dp[i][j], dp[i][k]+dp[i][j-k])
 			}
-			for k := 1; k < i; k++ {
+			for k := 1; k <= i/2; k++ {
 				dp[i][j] = max64(dp[i][j], dp[k][j]+dp[i-k][j])
 			}
 		}
