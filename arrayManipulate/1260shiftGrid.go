6c6379f8f01911fd1e36484a6629d9660b11d97e
binkesi <sjtusgn@163.com>
2022-07-20 10:54:13 +0800

    solve prob 1260 manuplate array

---
+++ arrayManipulate/1260shiftGrid.go
@@ -0,0 +1,18 @@
+package arraymanipulate
+
+// https://leetcode.cn/problems/shift-2d-grid/
+
+func shiftGrid(grid [][]int, k int) [][]int {
+	m, n := len(grid), len(grid[0])
+	ans := make([][]int, m)
+	for i := range ans {
+		ans[i] = make([]int, n)
+	}
+	for i, row := range grid {
+		for j, v := range row {
+			index1 := (i*n + j + k) % (m * n)
+			ans[index1/n][index1%n] = v
+		}
+	}
+	return ans
+}
